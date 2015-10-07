0b88f26e69116bcbe02e604e86cfd4d5c4f0e656
ypan887 <ypan887@gmail.com>
2015-10-07 01:41:44 -0700

    find two more space with inproper indentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
 
 
   private
-  def user_params
-    params.require(:user).permit(:email)
-  end
+    def user_params
+      params.require(:user).permit(:email)
+    end
 end
