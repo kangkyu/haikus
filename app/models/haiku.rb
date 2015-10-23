5682be8f74fc36f30a446a403b767d88972674e0
Charlie McElfresh <charlie.mcelfresh@socialchorus.com>
2015-10-22 17:29:35 -0700

    Add RandomHaiku subclass

--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -1,5 +1,12 @@
 class Haiku < ActiveRecord::Base
   has_many :lines, inverse_of: :haiku
   accepts_nested_attributes_for :lines
-  validates :lines, presence: true
+  # validates :lines, presence: true
+
+  before_create :say_hello
+
+  def say_hello
+    puts "hello"
+  end
+
 end
---
+++ app/models/random_haiku.rb
@@ -0,0 +1,3 @@
+class RandomHaiku < Haiku
+
+end
